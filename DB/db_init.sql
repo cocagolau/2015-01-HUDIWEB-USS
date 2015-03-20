68674ba70005a239cc869fdf27695585ed48e94b
woolimRyu <directorryu@gmail.com>
2015-03-20 11:14:08 +0900

    db update

--- DB/db_init.sql
+++ DB/db_init.sql
@@ -5,7 +5,7 @@ CREATE TABLE `User` (
 	`User_nickName` VARCHAR(255) NOT NULL DEFAULT '',
 	`User_password` VARCHAR(255) NOT NULL DEFAULT '',
 	`User_profile` text NOT NULL DEFAULT '',
-	
+	`User_gender` VARCHAR(255) NOT NULL DEFAULT '',
 	
 	PRIMARY KEY(`User_id`)
 ) ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
@@ -13,7 +13,7 @@ CREATE TABLE `User` (
 DROP TABLE IF EXISTS `UserPhoto`;
 CREATE TABLE `UserPhoto` (
 	`UserPhoto_id` integer auto_increment NOT NULL,
-	`UserPhoto_userId` integer NOT NULL DEFAULT '',
+	`UserPhoto_userId` integer NOT NULL DEFAULT 0,
 	`UserPhoto_url` text NOT NULL DEFAULT '',
 	PRIMARY KEY(`UserPhoto_id`)
 ) ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
@@ -21,10 +21,16 @@ CREATE TABLE `UserPhoto` (
 DROP TABLE IF EXISTS `UserPoint`;
 CREATE TABLE `UserPoint` (
 	`UserPoint_id` integer auto_increment NOT NULL,
-	`UserPoint_userId` integer NOT NULL DEFAULT '',
+	`UserPoint_userId` integer NOT NULL DEFAULT 0,
 	`UserPoint_url` text NOT NULL DEFAULT '',
 	PRIMARY KEY(`UserPhoto_id`)
 ) ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
 
-
-
+DROP TABLE IF EXISTS `Matching`;
+CREATE TABLE `Matching` (
+	`Matching_female` integer NOT NULL,
+	`Matching_male` integer NOT NULL DEFAULT 0,
+	`Matching_date` date NOT NULL,
+	
+	PRIMARY KEY(`Matching_female`, `Mathcing_male`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
