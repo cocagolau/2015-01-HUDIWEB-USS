5ebdacaaabea1c0e76a8b40690bbb79b26309299
byjo <buyongjo@gmail.com>
2015-04-09 11:14:34 +0900

    delete unused class

--- src/lib/mapping/annotation/Controller.java
+++
@@ -1,9 +0,0 @@
-package lib.mapping.annotation;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Controller {
-
-}
